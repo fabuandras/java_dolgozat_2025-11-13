7a69616edead7ee5e1a7c0acd22eab7cd2e6b8b2
fabuandras <fabuandras@gmail.com>
2025-11-13 14:33:38 +0100

    LightOnJatek (indito)

--- src/main/LightOnJatek.java
+++ src/main/LightOnJatek.java
@@ -2,10 +2,17 @@ package main;
 
 //Fabu András
 
+import model.LampaModell;
+import nezet.GUILightOnNezet;
+import vezerlo.LightOnVezerlo;
+
+
 public class LightOnJatek {
 
     public static void main(String[] args) {
-        
+        LampaModell modell = new LampaModell();
+        GUILightOnNezet nezet = new GUILightOnNezet();
+        LightOnVezerlo vezerlo = new LightOnVezerlo(modell, nezet);
     }
     
 }
